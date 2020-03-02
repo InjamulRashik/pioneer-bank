4d01d261b25ec808654c1a9866baaaa68623e66b
Injamul Haque Rashik <ihrashik@gmail.com>
2020-03-02 19:32:05 +0600

    Deposit Worked!

--- app.js
+++ app.js
@@ -12,23 +12,20 @@ const depositBtn = document.getElementById("deposit");
 depositBtn.addEventListener("click", function() {
   const depositAmount = document.getElementById("depositAmount").value;
   const depositNumber = parseFloat(depositAmount);
-  const currentDeposit = document.getElementById("currentDeposit").innerText;
-  const currentDepositNumber = parseFloat(currentDeposit);
-  const totalDeposit = depositNumber + currentDepositNumber;
 
-  document.getElementById("currentDeposit").innerText = totalDeposit;
+  //   const currentDeposit = document.getElementById("currentDeposit").innerText;
+  //   const currentDepositNumber = parseFloat(currentDeposit);
+  //   const totalDeposit = depositNumber + currentDepositNumber;
+  //   document.getElementById("currentDeposit").innerText = totalDeposit;
+  updateSpanText("currentDeposit", depositNumber);
+  updateSpanText("currentBalance", depositNumber);
   document.getElementById("depositAmount").value = "";
 });
+function updateSpanText(id, depositNumber) {
+  const currentBalance = document.getElementById(id).innerText;
+  const currentBalanceNumber = parseFloat(currentBalance);
+  const totalBalance = depositNumber + currentBalanceNumber;
+  document.getElementById(id).innerText = totalBalance;
+}
 
 //withdraw button event handler
-const depositBtn = document.getElementById("deposit");
-depositBtn.addEventListener("click", function() {
-  const depositAmount = document.getElementById("depositAmount").value;
-  const depositNumber = parseFloat(depositAmount);
-  const currentDeposit = document.getElementById("currentDeposit").innerText;
-  const currentDepositNumber = parseFloat(currentDeposit);
-  const totalDeposit = depositNumber + currentDepositNumber;
-
-  document.getElementById("currentDeposit").innerText = totalDeposit;
-  document.getElementById("depositAmount").value = "";
-});
