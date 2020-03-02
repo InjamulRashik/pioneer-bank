2668ff8bd02b406c14a8f6d0d41c02f190b31e11
Injamul Haque Rashik <ihrashik@gmail.com>
2020-03-02 19:03:16 +0600

    Deposit Button Done!

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
+//login button event handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function() {
   const loginArea = document.getElementById("login-area");
   loginArea.style.display = "none";
   const transArea = document.getElementById("transaction-area");
   transArea.style.display = "block";
+});
+
+//deposit button event handler
+const depositBtn = document.getElementById("deposit");
+depositBtn.addEventListener("click", function() {
+  const depositAmount = document.getElementById("depositAmount").value;
+  const depositNumber = parseFloat(depositAmount);
+  const currentDeposit = document.getElementById("currentDeposit").innerText;
+  const currentDepositNumber = parseFloat(currentDeposit);
+  const totalDeposit = depositNumber + currentDepositNumber;
+
+  document.getElementById("currentDeposit").innerText = totalDeposit;
+  document.getElementById("depositAmount").value = "";
+});
+
+//withdraw button event handler
+const depositBtn = document.getElementById("deposit");
+depositBtn.addEventListener("click", function() {
+  const depositAmount = document.getElementById("depositAmount").value;
+  const depositNumber = parseFloat(depositAmount);
+  const currentDeposit = document.getElementById("currentDeposit").innerText;
+  const currentDepositNumber = parseFloat(currentDeposit);
+  const totalDeposit = depositNumber + currentDepositNumber;
+
+  document.getElementById("currentDeposit").innerText = totalDeposit;
+  document.getElementById("depositAmount").value = "";
 });
